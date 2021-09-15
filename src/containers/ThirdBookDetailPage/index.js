eb027fdc2cd85a77d80315ced2d4122f33165416
Sachin796 <sachinjaveri.2010@gmail.com>
2021-09-14 20:06:01 -0400

    skeleton of app

---
+++ src/containers/ThirdBookDetailPage/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const ThirdBookDetailPage = () => {
+  return(
+    <>
+  <div className="page3Main">
+    <div className="page3Div1">Div1</div>
+    <div className="page3Div2">Div2</div>
+  </div>
+  <div>
+    <div className="page3Main2">
+      <div className="page3Div3">Div3</div>
+      <div className="page3Div4">Div4</div>
+    </div>
+  </div>
+</>
+  );
+};
+
+export default ThirdBookDetailPage;
